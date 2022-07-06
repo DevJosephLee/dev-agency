e760f5912c7b1aceb7c3d97971c4f9596a0ed0c6
Addy Abid <addy.abeed@gmail.com>
2022-07-06 21:50:07 +0000

    implemented navbar for desktop and mobile

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavBar from '../components/nav-bar';
+import Footer from '../components/footer';
 export default function Home(props) {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 }
