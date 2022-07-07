ddd458984662eaaf04df45fd5b59fb9ddd8426af
Addy Abid <addy.abeed@gmail.com>
2022-07-07 04:09:05 +0000

    added section image upload in progress

---
+++ client/components/home-mid.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const HomePageMid = () => {
+  return (
+    <div className='container'>
+      <div className='d-flex justify-content-center'>
+        <h3>Based in Orange County, CA</h3>
+      </div>
+      <div className='d-flex justify-content-left'>
+        <div className='col'>
+          <h4>Lorem Ipsum</h4>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.Reiciendis, itaque!Quia sapiente atque obcaecati in ad Quia sapiente atque obcaecati in ad </p>
+        </div>
+        <div className='col'></div>
+        <div className='d-flex justify-content-left'>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageMid;
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/nav-bar';
 import Footer from '../components/footer';
+import HomePageMid from '../components/home-mid';
 export default function Home(props) {
   return (
     <div>
       <NavBar />
+        <HomePageMid />
       <Footer />
     </div>
   );
