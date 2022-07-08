9b5abb8122d8fe4800bfa8ad12dcb2dbf715059d
Joseph Lee <teddylou0215@gmail.com>
2022-07-08 12:14:31 -0700

    mobile response

--- client/components/email-sub.jsx
+++ client/components/email-sub.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from '@emotion/styled';
 import Button from '@mui/material/Button';
 
 const StyledDiv = styled.div`
-  display: flex;
-  align-items: center;
   background-color: #1B1B1B;
   color: white;
   padding: 35px;
@@ -13,13 +11,39 @@ const StyledDiv = styled.div`
 `;
 
 export default function EmailSub() {
+  function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined
+    });
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+      }
+      // Add event listener
+      window.addEventListener('resize', handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
+
   return (
     <div className="container mt-5 mb-5">
       <div className="mb-5">
         <h1>Lorem Ipsum</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
-      <StyledDiv>
+      <StyledDiv className={useWindowSize().width > 991 ? 'd-flex align-items-center' : ''}>
         <div className="col">
           <h2>Stay in the loop</h2>
           <p>Subscribe to receive the latest news and updates about TDA.
